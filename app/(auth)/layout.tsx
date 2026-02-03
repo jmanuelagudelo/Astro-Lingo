6b9d20f0f4ee7088dd39b3b5afe26585274b186d
Juan Agudelo <jmanuel.agudelo1@udea.edu.co>
2026-02-03 09:06:29 -0500

    fixing deploy bugs

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import type { PropsWithChildren } from "react";
 
-import { Header } from "./header";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
 
       <main className="flex flex-1 flex-col items-center justify-center">
         {children}
